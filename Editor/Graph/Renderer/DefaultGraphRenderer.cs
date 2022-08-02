f28003b121424e45fecb4ab2fbb980a5305ef6a2
James B <Epicguru@users.noreply.github.com>
2022-08-02 18:00:22 +0200

    Update DefaultGraphRenderer.cs

--- Editor/Graph/Renderer/DefaultGraphRenderer.cs
+++ Editor/Graph/Renderer/DefaultGraphRenderer.cs
@@ -454,7 +454,7 @@ namespace GraphVisualizer
             return new Vector2((v.x + offset.x) * scaleFactor.x, (v.y + offset.y) * scaleFactor.y);
         }
         
-        private static readonly MethodInfo getBehaviourMethod = typeof(UnityEngine.Playables.PlayableHandle).GetMethod("GetScriptInstance", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+        private static readonly System.Reflection.MethodInfo getBehaviourMethod = typeof(UnityEngine.Playables.PlayableHandle).GetMethod("GetScriptInstance", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
 
         // Draw a node an return true if it has been clicked
         private void DrawNode(Rect nodeRect, Node node, bool selected)
